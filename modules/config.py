7835de762049b2646b7dcdcfce0d34f31b048d35
3d3n <96348438+3d3n-pyc@users.noreply.github.com>
2024-06-04 13:48:25 +0200

    Resolve issue #4

--- modules/config.py
+++ modules/config.py
@@ -2,12 +2,12 @@ import yaml
 import json
 
 def pluginData(plugin, file: str = 'config.yml', format: str = 'yml') -> dict:
-    f = open(f'plugin_data/{plugin}/{file}', 'r', encoding='UTF-8')
+    with open(f'plugin_data/{plugin}/{file}', 'r', encoding='UTF-8') as f:
 
-    if format == 'yml' or format == 'yaml':
-        return yaml.safe_load(f)
+        if format == 'yml' or format == 'yaml':
+            return yaml.safe_load(f)
 
-    if format == 'json':
-        return json.load(f)
+        if format == 'json':
+            return json.load(f)
     
-    return f.read()
+        return f.read()
